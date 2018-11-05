33c815f36fd6bb0ad91b2d103d87597e9c49e1cb
Josef Staroba <jstaroba@centene.com>
2018-11-05 16:17:35 -0600

    Add check for gutter game

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -1,10 +1,21 @@
 import { BowlingGame } from './index';
+
 describe('Bowling', () => {
   describe('all strikes', () => {
     it('should return a score of 300', () => {
       let perfectGameScore = 'X X X X X X X X X X X X';
       let expected = 300;
       let actual = BowlingGame.getScore(perfectGameScore);
+
+      expect(actual).toEqual(expected);
+    });
+  });
+
+  describe('all gutter balls', () => {
+    it('should return a score of 0', () => {
+      let gutterBallGameScore = '-- -- -- -- -- -- -- -- -- -- -- --';
+      let expected = 0;
+      let actual = BowlingGame.getScore(gutterBallGameScore);
 
       expect(actual).toEqual(expected);
     });
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,25 @@
 export class BowlingGame {
   static getScore(gameScore) {
-    return 300;
+    let gameScores = gameScore.split(' ');
+
+    if(BowlingGame.checkPerfectGame(gameScores)) {
+      return 300;
+    }
+
+    if(BowlingGame.checkGutterGame(gameScores)) {
+      return 0;
+    }
+  }
+
+  static checkGutterGame(gameScores) {
+    return !!gameScores.reduce(function(a, b){
+      return ((a === b) && (a === '--')) ? a : NaN;
+    });
+  }
+
+  static checkPerfectGame(gameScores) {
+    return !!gameScores.reduce(function(a, b){
+      return ((a === b) && (a === 'X')) ? a : NaN;
+    });
   }
 }
