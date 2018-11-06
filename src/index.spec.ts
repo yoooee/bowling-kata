4ea0aca1f4f453d1d6468f3e90bd9dd87cf42c69
Josef Staroba <jstaroba@centene.com>
2018-11-06 08:09:57 -0600

    Add functionality to calculate score of standard game

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -3,9 +3,9 @@ import { BowlingGame } from './index';
 describe('Bowling', () => {
   describe('all strikes', () => {
     it('should return a score of 300', () => {
-      let perfectGameScore = 'X X X X X X X X X X X X';
+      let GameScore = 'X X X X X X X X X X X X';
       let expected = 300;
-      let actual = BowlingGame.getScore(perfectGameScore);
+      let actual = BowlingGame.getScore(GameScore);
 
       expect(actual).toEqual(expected);
     });
@@ -13,9 +13,19 @@ describe('Bowling', () => {
 
   describe('all gutter balls', () => {
     it('should return a score of 0', () => {
-      let gutterBallGameScore = '-- -- -- -- -- -- -- -- -- -- -- --';
+      let GameScore = '-- -- -- -- -- -- -- -- -- -- -- --';
       let expected = 0;
-      let actual = BowlingGame.getScore(gutterBallGameScore);
+      let actual = BowlingGame.getScore(GameScore);
+
+      expect(actual).toEqual(expected);
+    });
+  });
+
+  describe('all 9s for roll 1 and gutter balls for roll 2', () => {
+    it('should return a score of 90', () => {
+      let GameScore = '9- 9- 9- 9- 9- 9- 9- 9- 9- 9-';
+      let expected = 90;
+      let actual = BowlingGame.getScore(GameScore);
 
       expect(actual).toEqual(expected);
     });
--- src/index.ts
+++ src/index.ts
@@ -4,10 +4,10 @@ export class BowlingGame {
 
     if(BowlingGame.checkPerfectGame(gameScores)) {
       return 300;
-    }
-
-    if(BowlingGame.checkGutterGame(gameScores)) {
+    } else if (BowlingGame.checkGutterGame(gameScores)) {
       return 0;
+    } else {
+      return BowlingGame.getStandardScore(gameScores);
     }
   }
 
@@ -21,5 +21,16 @@ export class BowlingGame {
     return !!gameScores.reduce(function(a, b){
       return ((a === b) && (a === 'X')) ? a : NaN;
     });
+  }
+
+
+  static getStandardScore(gameScores) {
+    let standardScore = gameScores.reduce(function(score, frame){
+      let rolls = frame.split('');
+      if(rolls[1] === '-') {
+        return score + +rolls[0];
+      }
+    }, 0);
+    return standardScore;
   }
 }
