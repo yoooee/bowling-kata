66338bdd007d5a5a22381a459e0d9e9241fc5a05
Josef Staroba <jstaroba@centene.com>
2018-11-07 12:36:09 -0600

    Starting over for the ??? time

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -1,31 +1,37 @@
 import { BowlingGame } from './index';
 
 describe('Bowling', () => {
+  let bowlingGame: BowlingGame;
+
+  beforeEach(() => {
+    bowlingGame = new BowlingGame();
+  });
+
   describe('all strikes', () => {
     it('should return a score of 300', () => {
       let GameScore = 'X X X X X X X X X X X X';
       let expected = 300;
-      let actual = BowlingGame.getScore(GameScore);
+      let actual = bowlingGame.getScore(GameScore);
 
       expect(actual).toEqual(expected);
     });
   });
 
-  describe('all gutter balls', () => {
+  fdescribe('all gutter balls', () => {
     it('should return a score of 0', () => {
       let GameScore = '-- -- -- -- -- -- -- -- -- -- -- --';
       let expected = 0;
-      let actual = BowlingGame.getScore(GameScore);
+      let actual = bowlingGame.getScore(GameScore);
 
       expect(actual).toEqual(expected);
     });
   });
 
-  fdescribe('all 9s for roll 1 and gutter balls for roll 2', () => {
+  describe('all 9s for roll 1 and gutter balls for roll 2', () => {
     it('should return a score of 90', () => {
       let gamescore = '9- 9- 9- 9- 9- 9- 9- 9- 9- 9-';
       let expected = 90;
-      let actual = BowlingGame.getScore(gamescore);
+      let actual = bowlingGame.getScore(gamescore);
 
       expect(actual).toEqual(expected);
     });
@@ -35,7 +41,47 @@ describe('Bowling', () => {
     it('should return a score of 90', () => {
       let gamescore = '52 52 52 52 52 52 52 52 52 52';
       let expected = 70;
-      let actual = BowlingGame.getScore(gamescore);
+      let actual = bowlingGame.getScore(gamescore);
+
+      expect(actual).toEqual(expected);
+    });
+  });
+
+  describe('all 5s and one spare', () => {
+    it('should return a score of 60', () => {
+      let gamescore = '5/ 50 50 50 50 50 50 50 50 50';
+      let expected = 60;
+      let actual = bowlingGame.getScore(gamescore);
+
+      expect(actual).toEqual(expected);
+    });
+  });
+
+  describe('all 5s and all spares except final frame', () => {
+    it('should return a score of 140', () => {
+      let gamescore = '5/ 5/ 5/ 5/ 5/ 5/ 5/ 5/ 5/ 50';
+      let expected = 140;
+      let actual = bowlingGame.getScore(gamescore);
+
+      expect(actual).toEqual(expected);
+    });
+  });
+
+  describe('veriety of values and all spares except final frame', () => {
+    it('should return a score of 144', () => {
+      let gamescore = '1/ 2/ 3/ 4/ 5/ 6/ 7/ 8/ 9/ 50';
+      let expected = 144;
+      let actual = bowlingGame.getScore(gamescore);
+
+      expect(actual).toEqual(expected);
+    });
+  });
+
+  describe('veriety of values and all spares including final frame', () => {
+    it('should return a score of 158', () => {
+      let gamescore = '1/ 2/ 3/ 4/ 5/ 6/ 7/ 8/ 9/ 5/9';
+      let expected = 158;
+      let actual = bowlingGame.getScore(gamescore);
 
       expect(actual).toEqual(expected);
     });
@@ -45,7 +91,7 @@ describe('Bowling', () => {
     it('should return a score of 120', () => {
       let gamescore = '2/ 2/ 2/ 2/ 2/ 2/ 2/ 2/ 2/ 2/2';
       let expected = 120;
-      let actual = BowlingGame.getScore(gamescore);
+      let actual = bowlingGame.getScore(gamescore);
 
       expect(actual).toEqual(expected);
     });
@@ -55,7 +101,7 @@ describe('Bowling', () => {
     it('should return a score of 150', () => {
       let gamescore = '54 72 9- X 81 X X 81 9- XXX';
       let expected = 150;
-      let actual = BowlingGame.getScore(gamescore);
+      let actual = bowlingGame.getScore(gamescore);
 
       expect(actual).toEqual(expected);
     });
--- src/index.ts
+++ src/index.ts
@@ -1,56 +1,30 @@
 export class BowlingGame {
-  static getScore(gameScore) {
-    let gameScores = gameScore.split(' ');
-    console.log('### New Game #####################################################');
-    if(BowlingGame.checkPerfectGame(gameScores)) {
-      return 300;
-    } else if (BowlingGame.checkGutterGame(gameScores)) {
-      return 0;
-    } else {
-      return BowlingGame.getStandardScore(gameScores);
-    }
-  }
+  private _gameScore: Array<string>;
 
-  static checkGutterGame(gameScores) {
-    return !!gameScores.reduce(function(a, b){
-      return ((a === b) && (a === '--')) ? a : NaN;
+  getScore(gameScore) {
+    const gameScoreArray = gameScore.split(' ');
+    let gameScorePoints = 0;
+
+    gameScoreArray.forEach((frame) => {
+      const frameArray = frame.split('');
+      let frameScore = 0;
+
+      frameArray.forEach((roll) => {
+        frameScore += this.getRollValue(roll);
+      });
+
+      gameScorePoints += frameScore;
     });
+
+    console.log('-----------------', gameScorePoints);
+    return gameScorePoints;
   }
 
-  static checkPerfectGame(gameScores) {
-    return !!gameScores.reduce(function(a, b){
-      return ((a === b) && (a === 'X')) ? a : NaN;
-    });
-  }
+  getRollValue(roll) {
+    if (roll === '-') { return 0; }
+    if ((roll === 'X') || (roll === '/')) { return 10; }
 
-  static getStandardScore(gameScores) {
-    let score: number = 0;
-
-    score = gameScores.reduce((gameScore, currentFrame, currentFrameIndex) => {
-      const rolls = BowlingGame.getRolls(currentFrame);
-
-      const currentFrameScore = rolls.reduce((frameScore, currentRoll, currentRollIndex) => {
-        return frameScore += +BowlingGame.getPointValue(currentRoll);
-      }, 0);
-      return gameScore + +currentFrameScore;
-    }, 0);
-
-    return score;
-  }
-
-  static getPointValue(roll) {
-    if(roll === '-') {
-      return 0;
-    } else if (roll === '/') {
-      return 10;
-    } else if (roll === 'X') {
-      return 10;
-    } else {
-      return roll;
-    }
-  }
-
-  static getRolls(frame) {
-    return frame.split(''); 
+    return roll;
   }
 }
+
