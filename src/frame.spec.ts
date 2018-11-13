fc0c02d900fb2e1a852730d3d701bde3943105cb
Josef Staroba <jstaroba@centene.com>
2018-11-13 14:49:40 -0600

    Add test

--- src/frame.spec.ts
+++ src/frame.spec.ts
@@ -2,10 +2,18 @@ import { Frame } from './frame';
 
 describe('Frame', () => {
   describe('total', () => {
-    it('returns the total of all rolls for a given frame', () => {
+    it('returns 5 when given a roll of 2 and 3', () => {
       const frame: Frame = new Frame(2, 3);
       const subject: number = frame.total;
       const expected: number = 5;
+
+      expect(subject).toEqual(expected);
+    });
+
+    it('returns 10 when given a roll of 10', () => {
+      const frame: Frame = new Frame(10);
+      const subject: number = frame.total;
+      const expected: number = 10;
 
       expect(subject).toEqual(expected);
     });
