5b65cc92da5b5af7553a41a1266ecd6b68d33161
Josef Staroba <jstaroba@centene.com>
2018-11-15 07:28:38 -0600

    Move score parser class into separate file

--- src/index.ts
+++ src/index.ts
@@ -1,50 +1,10 @@
 import { Frame } from './frame';
+import { ScoreParser } from './score-parser';
 
 const LAST_FRAME = 9;
 const MAX_FRAME_SCORE = 10;
 const STRIKE = 10;
-const GAME_SCORE_DELIMITER = ' ';
-const FRAME_ROLL_DELIMITER = '';
 
-class ScoreParser {
-  private _gameScores: Array<number>;
-
-  constructor (gameScores) {
-    this._gameScores = this.convertGameScoresToValues(gameScores.split(GAME_SCORE_DELIMITER));
-  }
-
-  convertGameScoresToValues(gameScore) {
-    return gameScore.map((frame) => {
-      return this.calculateRollsValue(frame.split(FRAME_ROLL_DELIMITER));
-    });
-  }
-
-  calculateRollsValue(rolls: Array<string>) {
-    return rolls.map((currentRoll, index) => {
-      if (this._isSpare(index, currentRoll))
-        return this._calculateSpareValue(rolls, currentRoll, index);
-      return this.getRollValue(currentRoll);
-    });
-  }
-  private _calculateSpareValue(rolls, currentRoll, index) {
-    return this.getRollValue(currentRoll) - this.getRollValue(rolls[index -1]);
-  }
-
-  getRollValue(roll) {
-    if (roll === '-') return 0;
-    if ((roll === 'X') || (roll === '/')) return 10;
-
-    return parseInt(roll);
-  }
-
-  private _isSpare(index, currentRoll) {
-    return ((index === 1) && (currentRoll === '/'));
-  }
-
-  gameScores() {
-    return this._gameScores;
-  }
-}
 
 export class BowlingGame {
   private _gameScores: Array<number>;
---
+++ src/score-parser.ts
@@ -0,0 +1,42 @@
+const GAME_SCORE_DELIMITER = ' ';
+const FRAME_ROLL_DELIMITER = '';
+
+export class ScoreParser {
+  private _gameScores: Array<number>;
+
+  constructor (gameScores) {
+    this._gameScores = this.convertGameScoresToValues(gameScores.split(GAME_SCORE_DELIMITER));
+  }
+
+  convertGameScoresToValues(gameScore) {
+    return gameScore.map((frame) => {
+      return this.calculateRollsValue(frame.split(FRAME_ROLL_DELIMITER));
+    });
+  }
+
+  calculateRollsValue(rolls: Array<string>) {
+    return rolls.map((currentRoll, index) => {
+      if (this._isSpare(index, currentRoll))
+        return this._calculateSpareValue(rolls, currentRoll, index);
+      return this.getRollValue(currentRoll);
+    });
+  }
+  private _calculateSpareValue(rolls, currentRoll, index) {
+    return this.getRollValue(currentRoll) - this.getRollValue(rolls[index -1]);
+  }
+
+  getRollValue(roll) {
+    if (roll === '-') return 0;
+    if ((roll === 'X') || (roll === '/')) return 10;
+
+    return parseInt(roll);
+  }
+
+  private _isSpare(index, currentRoll) {
+    return ((index === 1) && (currentRoll === '/'));
+  }
+
+  gameScores() {
+    return this._gameScores;
+  }
+}
